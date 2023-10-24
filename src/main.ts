6dc2263e7066631fc934c603e94bb17a24f4a736
leendrew <leendrew99@gmail.com>
2023-10-24 23:51:54 +0600

    feat: class-validator validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import helmet from 'helmet';
 import { AppModule } from './app.module';
 import { ConfigService } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
+  app.setGlobalPrefix('/api');
   app.use(helmet());
+  app.useGlobalPipes(new ValidationPipe());
+  const configService = app.get(ConfigService);
   await app.listen(configService.app.port);
 }
 bootstrap();
