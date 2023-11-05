17f5162e208780bf810451071bedd24a490cfc3f
leendrew <leendrew99@gmail.com>
2023-11-05 19:19:42 +0600

    fix: update dtos

--- src/api/task/dto/update-task.dto.ts
+++ src/api/task/dto/update-task.dto.ts
@@ -1,7 +1,10 @@
+import { IsOptional } from 'class-validator';
 import type { UpdateTaskPayload } from '../task.types';
 
 export class UpdateTaskDto implements UpdateTaskPayload {
+  @IsOptional()
   name?: string;
 
+  @IsOptional()
   isCompleted?: boolean;
 }
--- src/api/user/dto/update-user.dto.ts
+++ src/api/user/dto/update-user.dto.ts
@@ -1,4 +1,4 @@
-import { MinLength } from 'class-validator';
+import { IsOptional, MinLength } from 'class-validator';
 import type { UpdateUserPayload } from '../user.types';
 import {
   MIN_PASSWORD_LENGTH,
@@ -7,12 +7,15 @@ import {
 } from '../../../shared/constants';
 
 export class UpdateUserDto implements UpdateUserPayload {
+  @IsOptional()
   @MinLength(MIN_USER_NAME_LENGTH)
   name?: string;
 
+  @IsOptional()
   @MinLength(MIN_LOGIN_LENGTH)
   login?: string;
 
+  @IsOptional()
   @MinLength(MIN_PASSWORD_LENGTH)
   password?: string;
 }
