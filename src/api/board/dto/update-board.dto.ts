5535931183b8a7ecff17dc742e05eebf8ec80794
leendrew <leendrew99@gmail.com>
2023-11-05 19:19:33 +0600

    fix: update dtos

--- src/api/board/dto/update-board.dto.ts
+++ src/api/board/dto/update-board.dto.ts
@@ -1,9 +1,13 @@
+import { IsOptional } from 'class-validator';
 import type { UpdateBoardPayload } from '../board.types';
 
 export class UpdateBoardDto implements UpdateBoardPayload {
+  @IsOptional()
   name?: string;
 
+  @IsOptional()
   login?: string;
 
+  @IsOptional()
   password?: string;
 }
