9d1d342377c89a18189e8e21e6a443cc9a8fd5ed
leendrew <leendrew99@gmail.com>
2023-11-10 23:22:51 +0600

    chore: add return types

--- src/api/auth/auth.controller.ts
+++ src/api/auth/auth.controller.ts
@@ -1,24 +1,26 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import type { LoginDto, RefreshDto } from './dto';
+import type { TokenResponse } from './jwt';
 import type { CreateUserDto } from '../user/dto';
+import type { User } from '../user/user.entity';
 
 @Controller('/auth')
 export class AuthController {
   constructor(private readonly service: AuthService) {}
 
   @Post('/register')
-  register(@Body() dto: CreateUserDto) {
+  register(@Body() dto: CreateUserDto): Promise<User> {
     return this.service.register(dto);
   }
 
   @Post('/login')
-  login(@Body() dto: LoginDto) {
+  login(@Body() dto: LoginDto): Promise<TokenResponse> {
     return this.service.login(dto);
   }
 
   @Post('/refresh')
-  refresh(@Body() dto: RefreshDto) {
+  refresh(@Body() dto: RefreshDto): Promise<TokenResponse> {
     return this.service.refresh(dto);
   }
 }
