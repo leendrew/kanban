302accffe4c58bb5540908b7dd1761348bdd66db
leendrew <leendrew99@gmail.com>
2023-11-20 23:20:27 +0600

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -3,20 +3,19 @@ import { ValidationPipe } from '@nestjs/common';
 import helmet from 'helmet';
 import { AppModule } from './app.module';
 import { ConfigService } from './config';
+import { CORS_ORIGIN_ALL } from './shared';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  const config = app.get(ConfigService);
+  const originArray = config.cors.origin.split(',');
+  const origin = originArray.includes(CORS_ORIGIN_ALL) ? CORS_ORIGIN_ALL : originArray;
 
   app.setGlobalPrefix('/api');
   app.use(helmet());
   app.useGlobalPipes(new ValidationPipe());
-
-  const config = app.get(ConfigService);
-  const isDev = config.app.nodeEnv === 'development';
-
-  if (isDev) {
-    app.enableCors({ origin: config.cors.origin.split(',') });
-  }
+  app.enableCors({ origin });
 
   await app.listen(config.app.port);
 }
--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -3,3 +3,5 @@ export const MIN_PASSWORD_LENGTH = 4;
 export const MIN_LOGIN_LENGTH = 3;
 
 export const MIN_USER_NAME_LENGTH = 2;
+
+export const CORS_ORIGIN_ALL = '*';
